cf1e44719259f5f79e3bd7192de86c8e7833e93b
AnLe05 <vinhanle05@gmail.com>
2025-06-05 15:43:17 +0700

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :comments 
+  has_many :comments
 
-  validates :username, presence: true, uniqueness: true, length: {in: 4..12}
-  validates :email, presence: true, uniqueness: true 
+  validates :username, presence: true, uniqueness: true, length: { in: 4..12 }
+  validates :email, presence: true, uniqueness: true
 end
